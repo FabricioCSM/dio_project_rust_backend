9815bac5e99f4435b244475d89ba35e73d39b6c5
Fabricio Martins <fcmartins@voegol.com.br>
2024-03-29 10:36:02 -0300

    set and configura docker and database connection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 pub mod api;
+pub mod database;
 
+use database::Db;
 use rocket::launch;
+use rocket_db_pools::Database;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(Db::init())
         .attach(crate::api::hello_world::stage())
 }
